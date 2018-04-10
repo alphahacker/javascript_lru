532c30b8202190a3c3aa9d21598a9b0bc30f215f
한 승희 <alphahacker.han@gmail.com>
2018-04-10 13:58:52 +0900

    init commit

---
+++ basic_lru/app.js
@@ -0,0 +1,72 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var Promise = require('promise');
+
+console.log("app 1");
+//----------------------------------------------------------------//
+
+var kafka = require('./routes/kafka');
+
+//----------------------------------------------------------------//
+
+var app = express();
+console.log("app 2");
+//----------------------------------------------------------------//
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+//app.use(logger('dev'));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//----------------------------------------------------------------//
+console.log("app 3");
+app.use('/kafka', kafka);
+
+//----------------------------------------------------------------//
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+console.log("app 4");
+//----------------------------------------------------------------//
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+console.log("app 5");
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+console.log("app 6");
+//----------------------------------------------------------------//
+
+module.exports = app;
